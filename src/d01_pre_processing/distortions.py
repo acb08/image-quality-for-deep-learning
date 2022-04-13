4ab30dc707d48016b09a0e934f1921519a03e104
acb08 <acb6595@rit.edu>
2022-04-13 14:32:55 -0400

    updated distortions and distortion configs based on looking at fist scan result

--- src/d01_pre_processing/distortions.py
+++ src/d01_pre_processing/distortions.py
@@ -9,51 +9,51 @@ from PIL import Image
 RNG = np.random.default_rng()
 
 
-class AddPoissonNoise(object):
-    """
-    Adds zero-centered Poisson noise of standard deviation sigma_poisson.
-
-    sigma_poisson is intended to be specified in digital number to be added to an 8-bit image, but it is applied
-    assuming the image has already been scaled to fall on [0, 1], so the zero-centered Poisson distribution
-    is divided by 255 to match the input image scaling.
-    """
-
-    def __init__(self, sigma_poisson, clamp=True):
-        self.lambda_poisson = sigma_poisson ** 2
-        self.clamp = clamp
-
-    def __call__(self, tensor):
-        noise = (torch.poisson(torch.ones(tensor.shape) * self.lambda_poisson).float() - self.lambda_poisson) / 255
-        noisy_tensor = tensor + noise
-        if self.clamp:
-            noisy_tensor = torch.clamp(noisy_tensor, 0, 1)
-        return noisy_tensor
-
-
-class AddVariablePoissonNoise(object):
-    """
-    Selects Poisson noise standard deviation on sigma_poisson_range with uniform probability
-    inclusive of the endpoints.
-
-    generates zero-centered poisson noise with mean selected as described above, divides the noise by 255,
-    adds to input tensor and clamps the output to fall on [0, 1]
-
-
-
-    """
-
-    def __init__(self, sigma_poisson_range, clamp=True):
-        self.low = sigma_poisson_range[0] ** 2
-        self.high = sigma_poisson_range[1] ** 2 + 1  # add one because torch.randint excludes right endpoint
-        self.clamp = clamp
-
-    def __call__(self, tensor):
-        lambda_poisson = torch.randint(self.low, self.high, (1,))
-        noise = (torch.poisson(torch.ones(tensor.shape) * lambda_poisson).float() - lambda_poisson) / 255
-        noisy_tensor = tensor + noise
-        if self.clamp:
-            noisy_tensor = torch.clamp(noisy_tensor, 0, 1)
-        return noisy_tensor
+# class AddPoissonNoise(object):
+#     """
+#     Adds zero-centered Poisson noise of standard deviation sigma_poisson.
+#
+#     sigma_poisson is intended to be specified in digital number to be added to an 8-bit image, but it is applied
+#     assuming the image has already been scaled to fall on [0, 1], so the zero-centered Poisson distribution
+#     is divided by 255 to match the input image scaling.
+#     """
+#
+#     def __init__(self, sigma_poisson, clamp=True):
+#         self.lambda_poisson = sigma_poisson ** 2
+#         self.clamp = clamp
+#
+#     def __call__(self, tensor):
+#         noise = (torch.poisson(torch.ones(tensor.shape) * self.lambda_poisson).float() - self.lambda_poisson) / 255
+#         noisy_tensor = tensor + noise
+#         if self.clamp:
+#             noisy_tensor = torch.clamp(noisy_tensor, 0, 1)
+#         return noisy_tensor
+#
+#
+# class AddVariablePoissonNoise(object):
+#     """
+#     Selects Poisson noise standard deviation on sigma_poisson_range with uniform probability
+#     inclusive of the endpoints.
+#
+#     generates zero-centered poisson noise with mean selected as described above, divides the noise by 255,
+#     adds to input tensor and clamps the output to fall on [0, 1]
+#
+#
+#
+#     """
+#
+#     def __init__(self, sigma_poisson_range, clamp=True):
+#         self.low = sigma_poisson_range[0] ** 2
+#         self.high = sigma_poisson_range[1] ** 2 + 1  # add one because torch.randint excludes right endpoint
+#         self.clamp = clamp
+#
+#     def __call__(self, tensor):
+#         lambda_poisson = torch.randint(self.low, self.high, (1,))
+#         noise = (torch.poisson(torch.ones(tensor.shape) * lambda_poisson).float() - lambda_poisson) / 255
+#         noisy_tensor = tensor + noise
+#         if self.clamp:
+#             noisy_tensor = torch.clamp(noisy_tensor, 0, 1)
+#         return noisy_tensor
 
 
 class VariablePoissonNoiseChannelReplicated(object):
@@ -152,22 +152,53 @@ class VariableResolution(object):
         return transform_use(tensor)
 
 
-def n_scan(img):
+def _add_zero_centered_channel_replicated_poisson_noise(img, lambda_poisson):
+
+    img = np.asarray(img)
+    h, w, c = img.shape
+    noise = RNG.poisson(lambda_poisson, size=(h, w)) - lambda_poisson
+    noise_replicated = np.stack(c * [noise], axis=2)
+    img_out = img + noise_replicated
+    img_out = np.clip(img_out, 0, 255)
+
+    return img_out
+
+
+# n_scan did not replicate noise across channels
+# def n_scan(img):
+#     """
+#     :param img: image array, values on [0, 255]
+#     :return: image + zero centered Poisson noise, where the resulting image is clamped to fall on [0, 255]
+#
+#     """
+#
+#     img = np.asarray(img)
+#     shape = img.shape
+#
+#     sigma_poisson = np.random.randint(0, 26)  # add 1 to target distribution, high is "one above the highest integer
+#     to be drawn from the target distribution
+#     lambda_poisson = sigma_poisson ** 2
+#     noise = RNG.poisson(lambda_poisson, size=shape) - lambda_poisson
+#     img_out = img + noise
+#     img_out = np.clip(img_out, 0, 255)
+#
+#     return img_out, 'lambda_poisson', lambda_poisson
+
+
+def n_scan_v2(img):
     """
+    Adds zero-centered, channel-replicated Poisson noise up to 25 DN.
+
     :param img: image array, values on [0, 255]
     :return: image + zero centered Poisson noise, where the resulting image is clamped to fall on [0, 255]
 
     """
 
-    img = np.asarray(img)
-
-    shape = img.shape
-    sigma_poisson = np.random.randint(0, 26)  # add 1 to target distribution, high is "one above the highest integer to
-    # be drawn from the target distribution
+    sigma_max = 25
+    sigma_poisson = np.random.randint(0, sigma_max + 1)  # add 1 to target distribution, high is one above the highest
+    # integer to be drawn from the target distribution
     lambda_poisson = sigma_poisson ** 2
-    noise = RNG.poisson(lambda_poisson, size=shape) - lambda_poisson
-    img_out = img + noise
-    img_out = np.clip(img_out, 0, 255)
+    img_out = _add_zero_centered_channel_replicated_poisson_noise(img, lambda_poisson)
 
     return img_out, 'lambda_poisson', lambda_poisson
 
@@ -176,6 +207,15 @@ def b_scan(img):
 
     kernel_size = 23
     sigma_range = np.linspace(0.1, 4, num=21, endpoint=True)
+    std = np.random.choice(sigma_range)
+
+    return transforms.GaussianBlur(kernel_size=kernel_size, sigma=std)(img), 'std', std
+
+
+def b_scan_v2(img):
+
+    kernel_size = 17
+    sigma_range = np.linspace(0.1, 2.5, num=21, endpoint=True)
     std = np.random.choice(sigma_range)
 
     return transforms.GaussianBlur(kernel_size=kernel_size, sigma=std)(img), 'std', std
@@ -196,12 +236,30 @@ def r_scan():
     return transform
 
 
+def r_scan_v2():
+    """
+    Initializes and returns a VariableImageResize instance designed to re-size SAT6 images
+    randomly between 25% and 100% of original images size. Intended for use in dataset distortion (as opposed to in
+    a dataloader)
+    """
+
+    min_size = 4
+    max_size = 28
+    sizes = list(np.arange(min_size, max_size + 1))
+    transform = VariableImageResize(sizes)
+
+    return transform
+
+
 tag_to_image_distortion = {
     # _scan transforms intended for finding the point along each distortion axis at which performance of a
     # pre-trained model drops to chance
     'r_scan': r_scan,
+    'r_scan_v2': r_scan_v2,
     'b_scan': b_scan,
-    'n_scan': n_scan,
+    'b_scan_v2': b_scan_v2,
+    # 'n_scan': n_scan, # n_scan did not replicate noise over image channels
+    'n_scan_v2': n_scan_v2
 }
 
 tag_to_transform = {}
