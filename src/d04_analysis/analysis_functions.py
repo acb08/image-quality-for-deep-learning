083889abeaedd78938069e61a025b8a5d6714b30
acb08 <acb6595@rit.edu>
2022-04-21 10:40:27 -0400

    Removed extraneous comments (mostly obsolete functions)

--- src/d04_analysis/analysis_functions.py
+++ src/d04_analysis/analysis_functions.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def extract_combine_shard_vector_data(data, target_keys, check_keys=False):
@@ -110,67 +109,6 @@ def extract_embedded_vectors(data_dict,
         return distortion_vectors
 
 
-# class CompositeProps(object):
-#
-#     def __init__(self, properties,
-#                  parent_properties,
-#                  property_keys,
-#                  extractor,
-#                  iter_keys,
-#                  parent_iter_keys,
-#                  extractor_traverse_keys=None):
-#         self.props = properties
-#         self.parent_props = parent_properties
-#         self.property_keys = property_keys
-#         self.extractor = extractor
-#         self.iter_keys = iter_keys
-#         self.parent_iter_keys = parent_iter_keys
-#         self.traverse_keys = extractor_traverse_keys
-#         self.parameter_set = self.extractor(self.props, 'distortion_params', self.iter_keys)
-#
-#     def effective(self, property_key):
-#         prop = self.extractor(self.props, property_key, self.iter_keys, self.traverse_keys)
-#         parent_prop = self.extractor(self.parent_props, property_key, self.parent_iter_keys, self.traverse_keys)
-#         diff = np.asarray(prop) - np.asarray(parent_prop)
-#         effective_property = parent_prop - diff
-#         return effective_property
-#
-#     def all_effective_props(self):
-#         all_effective = {}
-#         for property_key in self.property_keys:
-#             all_effective[property_key] = self.effective(property_key)
-#         return all_effective
-#
-#     def all_effective_keys(self):
-#         return self.property_keys
-#
-#     def basic(self, property_key):
-#         prop = self.extractor(self.props, property_key, self.iter_keys, self.traverse_keys)
-#         return np.asarray(prop)
-#
-#     def parent(self, property_key):
-#         parent_prop = self.extractor(self.parent_props, property_key, self.parent_iter_keys, self.traverse_keys)
-#         return np.asarray(parent_prop)
-#
-#     def blur_values(self):
-#         stds = []
-#         for params in self.parameter_set:
-#             stds.append(params[2][1])
-#         return np.asarray(stds)
-#
-#     def noise_values(self):
-#         _noise_means = []
-#         for params in self.parameter_set:
-#             _noise_means.append(params[3][1])
-#         return np.asarray(_noise_means)
-#
-#     def res_values(self):
-#         _res_fractions = []
-#         for params in self.parameter_set:
-#             _res_fractions.append(params[1][1])
-#         return np.asarray(_res_fractions)
-
-
 def conditional_mean_accuracy(labels, predicts, condition_array):
 
     """
@@ -200,20 +138,6 @@ def conditional_mean_entropy(entropy, condition_array):
         conditional_entropies[i] = np.mean(entropy[np.where(condition_array == condition)])
 
     return conditions, conditional_entropies
-
-
-# def metric_perf_corrcoeff(performance, metric, hist):
-#     """
-#     Calculates the Pearson corelation coefficient between performance and metric
-#     using hist to appropriately weight the observations
-#     """
-#
-#     cov = np.cov(performance, metric, fweights=hist)
-#     diag = np.atleast_2d(np.diag(cov))
-#     dSqr = diag.T * diag
-#     corr = cov / np.sqrt(dSqr)
-#
-#     return corr[0, 1]
 
 
 def accuracy_v_entropy(image_entropies, labels, predicts,
@@ -273,69 +197,6 @@ def accuracy_v_entropy(image_entropies, labels, predicts,
 
     return (acc_vs_bin_index, occupied_entropy_bins, occupied_bin_left_edge_indices,
             entropy_bins, entropy_hist)
-
-
-# def plots_n_correlations(image_entropies, labels, predicts,
-#                          folder=None,
-#                          model=None,
-#                          dataset=None,
-#                          data_labels=None,
-#                          entropy_type=None,
-#                          thresh=0.01):
-#     """
-#     Local function
-#     """
-#
-#     acc_v_entropy, entropy_bins_used, entropy_bin_indices_used, bins, hist = (
-#         accuracy_v_entropy(image_entropies, labels, predicts))
-#
-#     plt.figure()
-#     plt.plot(entropy_bins_used, acc_v_entropy)
-#     xlabel = 'Effective ' + str(entropy_type) + ' Entropy'
-#     plt.xlabel(xlabel)
-#     plt.ylabel('Average Accuracy')
-#     title = str(model) + ' Model, ' + str(dataset)
-#     plt.title(title)
-#     name = title + ' ' + str(entropy_type) + '.png'
-#     plt.savefig(name)
-#
-#     histNormed = hist / np.sum(hist)
-#     acc_v_ent_thresh = acc_v_entropy[np.where(
-#         histNormed[entropy_bin_indices_used] >= thresh)]
-#     ent_bins_thresh = entropy_bins_used[np.where(
-#         histNormed[entropy_bin_indices_used] >= thresh)]
-#     plt.figure()
-#     plt.plot(ent_bins_thresh, acc_v_ent_thresh)
-#     xlabel = 'Effective ' + str(entropy_type) + ' Entropy'
-#     plt.xlabel(xlabel)
-#     plt.ylabel('Average Accuracy')
-#     title = str(model) + ' Model, ' + str(dataset) + \
-#             ' (bin weight >' + str(thresh) + ')'
-#     plt.title(title)
-#     name = (str(model) + ' Model, ' + str(dataset) + ' ' + str(entropy_type)
-#             + ' thresh (' + str(thresh) + ').png')
-#     # plt.savefig(name)
-#
-#     rho = metric_perf_corrcoeff(acc_v_entropy, entropy_bins_used,
-#                                 hist[entropy_bin_indices_used])
-#     print(model, 'model,', dataset, 'dataset,',
-#           entropy_type, 'entropy, rho =', rho)
-#
-#     return rho
-#
-#
-# def top_k_accuracy_vector(ground_truth, predictions):
-#     """
-#     ground_truth: array of length n containing ground truth labels
-#     predictions: k x n array containing top-k predictions for n samples
-#     returns: top k accuracy, where k is specified by shape of the predictions array.
-#
-#     """
-#
-#     broadcaster = np.multiply(ground_truth, np.ones_like(predictions))
-#     comparison = np.equal(broadcaster, predictions)
-#
-#     return comparison[0, :], np.sum(comparison, axis=0)
 
 
 def conditional_extract_2d(x, y, z):
