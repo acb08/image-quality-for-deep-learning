12768ccfb9a36201b4acb45e17d3f1d7fbcae22c
acb08 <acb6595@rit.edu>
2022-06-09 09:41:24 -0400

    print tweak

--- src/d00_utils/functions.py
+++ src/d00_utils/functions.py
@@ -214,9 +214,9 @@ def load_wandb_data_artifact(run, artifact_id, artifact_filename, retries=10):
         try:
             artifact = run.use_artifact(artifact_id)
         except ValueError:
+            print(f'wandb artifact access failed attempt {attempts}')
             attempts += 1
             time.sleep(60)
-            print(f'wandb artifact access failed attempt {attempts}')
         else:
             success = True
 
