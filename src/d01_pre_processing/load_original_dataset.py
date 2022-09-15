7401e56fdcee981712452aa12108b92b57a4e711
acb08 <acb6595@rit.edu>
2022-09-14 21:14:35 -0400

    fixed quick dataset load problem

--- src/d01_pre_processing/load_original_dataset.py
+++ src/d01_pre_processing/load_original_dataset.py
@@ -2,7 +2,8 @@ import scipy.io
 from pathlib import Path
 from src.d00_utils.definitions import ROOT_DIR, ORIGINAL_DATASETS
 
-dataset_path = Path(ROOT_DIR, ORIGINAL_DATASETS['sat6_full']['rel_path'])
+dataset_path = Path(ROOT_DIR, ORIGINAL_DATASETS['sat6_full']['rel_path'],
+                    ORIGINAL_DATASETS['sat6_full']['names_labels_filename'])
 dataset = scipy.io.loadmat(dataset_path)
 
 train_x, train_y = dataset['train_x'], dataset['train_y']
