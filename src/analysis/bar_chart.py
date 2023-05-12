99eae52438f01b49d6a6837748662685ff91a59a
acb08 <acb6595@rit.edu>
2023-05-12 17:51:11 -0400

    config sync

--- src/analysis/bar_chart.py
+++ src/analysis/bar_chart.py
@@ -361,7 +361,7 @@ def main(run_config):
 
 if __name__ == '__main__':
 
-    config_filename = 'pl_keeper-model_dw_3d.yml'
+    config_filename = 's6_keeper-model_dw_3d.yml'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_name', default=config_filename, help='config filename to be used')
